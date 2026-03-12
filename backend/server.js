e5807a13308c1d5c4a651ae874beb72308c4fb6b
Umesh I Ramawickrama <duiramawickrama@gmail.com>
2026-03-12 15:11:51 +0530

    Update backend server

--- backend/server.js
+++ backend/server.js
@@ -29,12 +29,13 @@ if (config.NODE_ENV !== 'production') {
   const devOrigins = [
     'http://localhost:3000',
     'http://localhost:3001',
+    'http://localhost:3002',
     'http://localhost:5173',
     'http://127.0.0.1:3000',
     'http://127.0.0.1:3001',
     'http://127.0.0.1:5173',
-    'http://13.48.136.109:3001',
-    'http://172.31.36.202:3001',
+    'http://13.48.136.109:3002',
+    'http://172.31.36.202:3002',
 
   ];
   devOrigins.forEach(origin => {
