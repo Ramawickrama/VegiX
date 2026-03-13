ddf99e515be56ee15610f507c6e80af539d5e1dd
Umesh I Ramawickrama <duiramawickrama@gmail.com>
2026-03-13 10:20:30 +0530

    chore: update environment configuration files

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
     port: 3000,
     proxy: {
       '/api': {
-        target: 'http://localhost:5000',
+        target: 'http://13.48.136.109:5000',
         changeOrigin: true,
       },
     },
